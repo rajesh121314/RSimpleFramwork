60189f772c2f9249ccf3cebf85e6885b4c1109b5
Rajesh <rajesh.s@vrinsoft.com>
2023-11-06 12:58:32 +0530

    Initial Commit

---
+++ RSimpleFramwork/RSimpleFramwork.h
@@ -0,0 +1,18 @@
+//
+//  RSimpleFramwork.h
+//  RSimpleFramwork
+//
+//  Created by vrinsoft on 06/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RSimpleFramwork.
+FOUNDATION_EXPORT double RSimpleFramworkVersionNumber;
+
+//! Project version string for RSimpleFramwork.
+FOUNDATION_EXPORT const unsigned char RSimpleFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSimpleFramwork/PublicHeader.h>
+
+
